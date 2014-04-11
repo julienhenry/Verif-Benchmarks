539ab8e9e08d66fb5374a7d9db11539126b616c5
Julien Henry <Julien.Henry@imag.fr>
2014-04-11 20:06:33 +0200

    scripts

--- SV-COMP-loop/trex03_true-unreach-label.c
+++ SV-COMP-loop/trex03_true-unreach-label.c
@@ -12,6 +12,9 @@ _Bool __VERIFIER_nondet_bool();
 int main()
 {
   unsigned int x1=__VERIFIER_nondet_uint(), x2=__VERIFIER_nondet_uint(), x3=__VERIFIER_nondet_uint();
+  while(x1 < 0){}
+  while(x2 < 0){}
+  while(x3 < 0){}
   unsigned int d1=1, d2=1, d3=1;
   _Bool c1=__VERIFIER_nondet_bool(), c2=__VERIFIER_nondet_bool();
   
